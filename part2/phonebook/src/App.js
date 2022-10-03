686cc7bb13adca89f4967272dff65192392d0e4f
Lucas Sjoberg <lucasjoberg@gmail.com>
2022-10-03 13:27:59 +0300

    courseInfo done and Phonebook step1 done

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import Person from "./components/Person"
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addNewName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+    name: newName,
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={handlePersonChange}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <Person key={person.name} person={person} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
